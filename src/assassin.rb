066ffb7b64c0908ecb64299212bbcd202b6c6de2
Zafer CAKMAK <mzcakmak@gmail.com>
2010-12-08 14:56:19 +0200

    added colorful output.
    - tasks have color by their priority
    - there is colorful output now

--- src/assassin.rb
+++ src/assassin.rb
@@ -1,9 +1,24 @@
 require 'yaml'
 
+def colorize(text, color_code)
+	  "\t#{color_code}#{text}\e[0m"
+	  end
+
+def red(text); colorize(text, "\e[31m"); end
+def green(text); colorize(text, "\e[32m"); end
+def blue(text); colorize(text, "\e[34m"); end
+def yellow(text); colorize(text, "\e[33m"); end
+
 yaml_file = YAML::load(File.open('tasks.yml'))
 yaml_file.each{ |category, tasks|
 	puts category
 	tasks.each{ |name, pri|
-		puts name + "has pri of " + pri
+		if pri == "low"
+			puts green(name) 
+		elsif pri == "high"
+			puts red(name)
+		elsif pri == "normal"
+			puts yellow(name)
+		end
 	}
 }
